f7e37cf819e16cbfcf0185cac4d279cfa9833fff
iarmankhan <armankhan9244@gmail.com>
2020-02-17 19:56:28 +0530

    Merge conflict solved

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import HomePage from './HomePage.component';
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <HomePage />
-=======
-      Hello World
->>>>>>> 0099bb21d7701924ed5e8a116ec307db46f4234a
     </div>
   );
 }
