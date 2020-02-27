ce8189fa54226d4ea9f9ccea4662df8ccc4ccfb5
iarmankhan <armankhan9244@gmail.com>
2020-02-27 16:08:30 +0530

    Using mapDispatchToProps to setCurrentUser after sing in or out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import React, {
-  Component
-} from 'react';
-import {
-  Switch,
-  Route
-} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux'
 
 import './App.css';
 
@@ -13,40 +9,31 @@ import Shop from './pages/shop/Shop.component';
 import Header from './components/header/Header.component';
 import Authentication from './pages/authentication/Authentication.component';
 
-import {
-  auth,
-  createUserProfileDocument
-} from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+import { setCurrentUser } from './redux/user/user.actions'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      currentUser: null
-    }
-  }
-
+ 
   unsubscribedFromAuth = null
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
+
     this.unsubscribedFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
         })
       }
 
-      this.setState({
-        currentUser: userAuth
-      })
+      setCurrentUser(userAuth)
     })
   }
 
@@ -68,4 +55,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
