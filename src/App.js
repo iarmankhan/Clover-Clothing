74cb19c219203fd9a9f9526b8dafecd91a2afd9f
iarmankhan <armankhan9244@gmail.com>
2020-02-27 15:55:28 +0530

    Using connect and mapStateToProps to pass current user to header

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={Shop} />
