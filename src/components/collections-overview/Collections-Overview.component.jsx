784d28e716d7b81b7be97c81d00828c05ee72140
iarmankhan <armankhan9244@gmail.com>
2020-03-06 00:10:06 +0530

    Fixed shop page error by creating new selector for turning object to array

--- src/components/collections-overview/Collections-Overview.component.jsx
+++ src/components/collections-overview/Collections-Overview.component.jsx
@@ -6,7 +6,7 @@ import './Collections-Overview.styles.scss'
 
 import CollectionPreview from '../../components/collection-preview/Collection-Preview.component';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 const CollectionsOverview = ({ collections }) => {
     return (
@@ -21,7 +21,7 @@ const CollectionsOverview = ({ collections }) => {
 }
  
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview);
