6447c0802226bcffb640c34dd7a836f4a410a12d
iarmankhan <armankhan9244@gmail.com>
2020-03-21 09:54:40 +0530

    Moved collections to firestore using firebase util

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux'
+import React, {Component} from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux'
 import {createStructuredSelector} from 'reselect'
 
 import './App.css';
@@ -11,60 +11,59 @@ import Header from './components/header/Header.component';
 import Authentication from './pages/authentication/Authentication.component';
 import Checkout from './pages/checkout/Checkout.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
-import { setCurrentUser } from './redux/user/user.actions'
-import { selectCurrentUser } from './redux/user/user.selectors'
+import {setCurrentUser} from './redux/user/user.actions'
+import {selectCurrentUser} from './redux/user/user.selectors'
 
 class App extends Component {
+    unsubscribedFromAuth = null;
 
- 
-  unsubscribedFromAuth = null
+    componentDidMount() {
+        const {setCurrentUser} = this.props;
 
-  componentDidMount() {
-    const {setCurrentUser} = this.props;
+        this.unsubscribedFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
 
-    this.unsubscribedFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+                userRef.onSnapshot(snapShot => {
+                    setCurrentUser({
+                        id: snapShot.id,
+                        ...snapShot.data()
+                    });
+                })
+            }
 
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data()
-          });
+            setCurrentUser(userAuth)
         })
-      }
+    }
 
-      setCurrentUser(userAuth)
-    })
-  }
+    componentWillUnmount() {
+        this.unsubscribedFromAuth()
+    }
 
-  componentWillUnmount() {
-    this.unsubscribedFromAuth()
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={Shop} />
-          <Route exact path='/checkout' component={Checkout} />
-          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<Authentication />)} />
-        </Switch>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Switch>
+                    <Route exact path='/' component={HomePage}/>
+                    <Route path='/shop' component={Shop}/>
+                    <Route exact path='/checkout' component={Checkout}/>
+                    <Route exact path='/signin'
+                           render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<Authentication/>)}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser : selectCurrentUser
-})
+    currentUser: selectCurrentUser
+});
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,7 +16,7 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
 
     const snapShot = await userRef.get();
 
@@ -39,6 +39,19 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     return userRef;
 };
 
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    console.log(collectionRef);
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj)
+    });
+
+    return await batch.commit();
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
@@ -46,7 +59,7 @@ export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({
-    prompt: 'select_account', 
+    prompt: 'select_account',
 });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
