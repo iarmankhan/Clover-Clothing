e340b55323208c8cad8c951ee74f9d013480753f
iarmankhan <armankhan9244@gmail.com>
2020-03-22 13:44:46 +0530

    Bringing data from firestore to our app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -52,6 +52,20 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
     return await batch.commit();
 };
 
+export const convertCollectionSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const {title, items} = doc.data();
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    });
+
+    console.log(transformedCollection)
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/shop/Shop.component.jsx
+++ src/pages/shop/Shop.component.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/Collections-Overview.component';
 import Collection from '../collection/Collection.component';
+import {firestore, convertCollectionSnapshotToMap} from "../../firebase/firebase.utils";
 
-const Shop = ({match}) => {
-    return (
-        <div className="shop-page">
-            <Route exact path={`${match.path}`} component={CollectionsOverview} />
-            <Route path={`${match.path}/:collectionId`} component={Collection} />
-        </div>
-    );
+class Shop extends Component{
+    unsubscribeFromSnapshot = null;
+
+    componentDidMount() {
+        const collectionRef = firestore.collection('collections');
+
+        collectionRef.onSnapshot(async snapshot => {
+            convertCollectionSnapshotToMap(snapshot)
+        })
+    }
+
+    render(){
+        const {match} = this.props;
+        return (
+            <div className="shop-page">
+                <Route exact path={`${match.path}`} component={CollectionsOverview} />
+                <Route path={`${match.path}/:collectionId`} component={Collection} />
+            </div>
+        );
+    }
 }
 
 export default Shop;
