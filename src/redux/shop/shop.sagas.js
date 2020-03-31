fa2cbdb810d03a8b5c5266f7203aae9ee0aceb64
iarmankhan <armankhan9244@gmail.com>
2020-03-31 22:17:18 +0530

    Created root saga - added it to store

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import {takeEvery, call, put} from 'redux-saga/effects'
+import {takeLatest, call, put} from 'redux-saga/effects'
 
 import ShopActionTypes from "./shop.types";
 import {convertCollectionSnapshotToMap, firestore} from "../../firebase/firebase.utils";
@@ -17,5 +17,5 @@ export function* fetchCollectionsAsync() {
 }
 
 export function* fetchCollectionsStart() {
-    yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync);
+    yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync);
 }
