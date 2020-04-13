70bb12f875f1c9eba7a78e9e066574beec7c106f
iarmankhan <armankhan9244@gmail.com>
2020-04-13 22:52:22 +0530

    Converted shop component to use useEffect instead of componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux'
 import {createStructuredSelector} from 'reselect'
-import { ToastContainer } from 'react-toastify';
+import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import './App.css';
@@ -17,40 +17,34 @@ import {selectCurrentUser} from './redux/user/user.selectors'
 import {checkUserSession} from "./redux/user/user.actions";
 import ScrollToTop from "./components/Utilities/ScrollToTop.component";
 
-class App extends Component {
-    unsubscribedFromAuth = null;
+const App = ({checkUserSession, currentUser}) => {
 
-    componentDidMount() {
-        const {checkUserSession} = this.props;
-
+    useEffect(() => {
         checkUserSession();
-    }
+    }, [checkUserSession]);
 
-    render() {
-        const {currentUser} = this.props;
-        return (
-            <div>
-                <ToastContainer />
-                <ScrollToTop />
-                <Header/>
-                <Switch>
-                    <Route exact path='/' component={HomePage}/>
-                    <Route path='/shop' component={Shop}/>
-                    <Route exact path='/checkout' component={Checkout}/>
-                    <Route exact path='/signin'
-                           render={() => currentUser ? (<Redirect to='/'/>) : (<Authentication/>)}/>
-                </Switch>
-            </div>
-        );
-    }
-}
+    return (
+        <div>
+            <ToastContainer/>
+            <ScrollToTop/>
+            <Header/>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/shop' component={Shop}/>
+                <Route exact path='/checkout' component={Checkout}/>
+                <Route exact path='/signin'
+                       render={() => currentUser ? (<Redirect to='/'/>) : (<Authentication/>)}/>
+            </Switch>
+        </div>
+    );
+};
 
 const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
-   checkUserSession: () => dispatch(checkUserSession())
+    checkUserSession: () => dispatch(checkUserSession())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/pages/shop/Shop.component.jsx
+++ src/pages/shop/Shop.component.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect} from 'react';
 import {Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 
@@ -7,29 +7,25 @@ import {fetchCollectionsStart} from "../../redux/shop/shop.actions";
 import CollectionsOverviewContainer from "../../components/collections-overview/Collections-Overview.container";
 import CollectionPageContainer from "../collection/Collection.container";
 
-class Shop extends Component{
-    componentDidMount() {
-        const {fetchCollectionsStart} = this.props
+const Shop = ({fetchCollectionsStart, match}) => {
+    useEffect(() => {
         fetchCollectionsStart()
-    }
+    }, [fetchCollectionsStart]);
 
-    render(){
-        const {match} = this.props;
-        return (
-            <div className="shop-page">
-                <Route
-                    exact
-                    path={`${match.path}`}
-                    component={CollectionsOverviewContainer}
-                />
-                <Route
-                    path={`${match.path}/:collectionId`}
-                    component={CollectionPageContainer}
-                />
-            </div>
-        );
-    }
-}
+    return (
+        <div className="shop-page">
+            <Route
+                exact
+                path={`${match.path}`}
+                component={CollectionsOverviewContainer}
+            />
+            <Route
+                path={`${match.path}/:collectionId`}
+                component={CollectionPageContainer}
+            />
+        </div>
+    );
+};
 
 
 const mapDispatchToProps = dispatch => {
